1644883a81afb4cd2ca0ab1d91bf94be0ed1a5b8
EvgenEvseev <333evseev@gmail.com>
2017-08-14 18:18:38 +0400

    Commit 2

---
+++ src/Chapter04/BasicMath.java
@@ -0,0 +1,23 @@
+package Chapter04;
+
+public class BasicMath {
+    public static void main(String args[]){
+        //арифметические операции над целочисленными значениями
+        System.out.println("Целочисленная арифметика");
+        int a=1+1,b=a*3,c=b/4,d=c-a,e=-d;
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+        System.out.println("d= "+d);
+        System.out.println("e= "+e);
+        // Арифметические операции над значениями типа double
+        System.out.println("\nАрифметика с плавающей точкой");
+        double da=1+1,db=da*3,dc=db/4,dd=dc-a,de=-dd;
+        System.out.println("da= "+da);
+        System.out.println("db= "+db);
+        System.out.println("dc= "+dc);
+        System.out.println("dd= "+dd);
+        System.out.println("de= "+de);
+        }
+    }
+
