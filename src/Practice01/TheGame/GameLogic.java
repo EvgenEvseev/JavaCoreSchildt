8281087476500806aaeddf4375013206681b8d1b
EvgenEvseev <333evseev@gmail.com>
2017-09-06 01:43:39 +0400

    Commit11

--- src/Practice01/TheGame/GameLogic.java
+++ src/Practice01/TheGame/GameLogic.java
@@ -13,6 +13,7 @@ public class GameLogic {
         System.out.println("|" + "4" + "|" + "5" + "|" + "6" + "|");
         System.out.println("|" + "7" + "|" + "8" + "|" + "9" + "|");
         for (; ; ) {
+            System.out.println("Ваш ход");
             int n = scan.nextInt() - 1;
             if (!Objects.equals(Field.mass[n], "x") & !Objects.equals(Field.mass[n], "o"))
                 Field.mass[n] = "x";
@@ -27,33 +28,10 @@ public class GameLogic {
                             ((Objects.equals(Field.mass[6], "x") & Objects.equals(Field.mass[7], "x")) & Objects.equals(Field.mass[8], "x")) ||
                             ((Objects.equals(Field.mass[0], "x") & Objects.equals(Field.mass[3], "x")) & Objects.equals(Field.mass[6], "x")) ||
                             ((Objects.equals(Field.mass[1], "x") & Objects.equals(Field.mass[4], "x")) & Objects.equals(Field.mass[7], "x")) ||
-                            ((Objects.equals(Field.mass[2], "x") & Objects.equals(Field.mass[5], "x")) & Objects.equals(Field.mass[9], "x")) ||
-                            ((Objects.equals(Field.mass[2], "x") & Objects.equals(Field.mass[4], "x")) & Objects.equals(Field.mass[7], "x")) ||
-                            ((Objects.equals(Field.mass[0], "x") & Objects.equals(Field.mass[4], "x")) & Objects.equals(Field.mass[9], "x"))) {
+                            ((Objects.equals(Field.mass[2], "x") & Objects.equals(Field.mass[5], "x")) & Objects.equals(Field.mass[8], "x")) ||
+                            ((Objects.equals(Field.mass[2], "x") & Objects.equals(Field.mass[4], "x")) & Objects.equals(Field.mass[6], "x")) ||
+                            ((Objects.equals(Field.mass[0], "x") & Objects.equals(Field.mass[4], "x")) & Objects.equals(Field.mass[8], "x"))) {
                 System.out.println("Победа Игрока!");
-                break;
-            }
-
-            System.out.println("Ход ИИ");
-            re:
-            {
-                int ran = rand.nextInt(9);
-                if (!Objects.equals(Field.mass[ran], "x") & !Objects.equals(Field.mass[ran], "o"))
-                    Field.mass[ran] = "o";
-                else break re;{
-                }
-            }
-            Field.show2();
-            if (
-                    ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[1], "o")) & Objects.equals(Field.mass[2], "o")) ||
-                            ((Objects.equals(Field.mass[3], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[5], "o")) ||
-                            ((Objects.equals(Field.mass[6], "o") & Objects.equals(Field.mass[7], "o")) & Objects.equals(Field.mass[8], "o")) ||
-                            ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[3], "o")) & Objects.equals(Field.mass[6], "o")) ||
-                            ((Objects.equals(Field.mass[1], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[7], "o")) ||
-                            ((Objects.equals(Field.mass[2], "o") & Objects.equals(Field.mass[5], "o")) & Objects.equals(Field.mass[9], "o")) ||
-                            ((Objects.equals(Field.mass[2], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[7], "o")) ||
-                            ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[9], "o"))) {
-                System.out.println("Победа ИИ!");
                 break;
             }
             if ((Objects.equals(Field.mass[0], "x") ^ Objects.equals(Field.mass[0], "o")) & (Objects.equals(Field.mass[1], "x") ^ Objects.equals(Field.mass[1], "o")) &
@@ -62,6 +40,29 @@ public class GameLogic {
                     (Objects.equals(Field.mass[6], "x") ^ Objects.equals(Field.mass[6], "o")) & (Objects.equals(Field.mass[7], "x") ^ Objects.equals(Field.mass[7], "o")) &
                     (Objects.equals(Field.mass[8], "x") ^ Objects.equals(Field.mass[8], "o"))) {
                 System.out.println("Ничья!");
+                break;
+            }
+
+            System.out.println("Ход ИИ");
+
+            for(;;){int ran = rand.nextInt(9);
+                if (!Objects.equals(Field.mass[ran], "x") & !Objects.equals(Field.mass[ran], "o")){
+                    Field.mass[ran] = "o";
+                    break;
+                }
+                else continue;}
+
+            Field.show2();
+            if (
+                    ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[1], "o")) & Objects.equals(Field.mass[2], "o")) ||
+                            ((Objects.equals(Field.mass[3], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[5], "o")) ||
+                            ((Objects.equals(Field.mass[6], "o") & Objects.equals(Field.mass[7], "o")) & Objects.equals(Field.mass[8], "o")) ||
+                            ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[3], "o")) & Objects.equals(Field.mass[6], "o")) ||
+                            ((Objects.equals(Field.mass[1], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[7], "o")) ||
+                            ((Objects.equals(Field.mass[2], "o") & Objects.equals(Field.mass[5], "o")) & Objects.equals(Field.mass[8], "o")) ||
+                            ((Objects.equals(Field.mass[2], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[6], "o")) ||
+                            ((Objects.equals(Field.mass[0], "o") & Objects.equals(Field.mass[4], "o")) & Objects.equals(Field.mass[8], "o"))) {
+                System.out.println("Победа ИИ!");
                 break;
             }
         }
