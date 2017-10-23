2c0c7c62c34b9029eb3671a0f3596ddf200b5972
EvgenEvseev <333evseev@gmail.com>
2017-10-23 14:27:48 +0400

    Commit11

---
+++ src/Practice02/Task1.java
@@ -0,0 +1,97 @@
+package Practice02;
+import java.util.*;
+/*
+addInc(индекс, значение) и addInc(значение) - добавление элемента.
+remDec(индекс) и remDec(значение)- удаление элемента.
+find(индекс) и find(значение)- поиск элемента.
+aver - среднее арифметическое
+
+ */
+ class Colletion01 extends ArrayList<Integer> {
+
+     public void addInc(int index, Integer element) {
+         if(element==null) System.out.println("В коллекции не допускается значение null");
+         else
+         add(0);
+         for (int i = 0; i < size(); i++) {
+             set(i, get(i) + element);
+         }
+     }
+
+     public void addInc(Integer element) {
+         if(element==null) System.out.println("В коллекции не допускается значение null");
+         else
+         add(0);
+         for (int i = 0; i < size(); i++) {
+             set(i, get(i) + element);
+         }
+     }
+
+     public void remDec(int index){
+         int j=get(index);
+         remove(index);
+         for (int i = 0; i < size(); i++) {
+             set(i, get(i) - j);
+         }
+     }
+
+     public void remDec(Integer element){
+         if(remove(element))
+         for (int i = 0; i < size(); i++)
+             set(i, get(i) - element);
+         else System.out.println("Элемент "+element+" для удаления не найден");
+     }
+
+     public void find(Integer element){
+         for(int i=0;i<size();i++){
+             if(element==get(i)) System.out.println("Элемент найден по индексу : "+i);
+             else System.out.println("\nЭлемент не найден");
+             break;
+         }
+     }
+
+     public void find(int index){
+         if(index>size()) System.out.println("\nРазмер динамического массива меньше чем "+index);
+         else
+        System.out.print("Элемент по индексу "+index+" имеет значение "+get(index));
+     }
+
+     public void aver(){
+         int sum=0;
+         for(int i=0;i<size();i++)sum+=get(i);
+         int ave=sum/size();
+         System.out.println("Среднее арифметическое дин.массива равно "+ave);
+     }
+ }
+
+
+
+public class Task1 {
+    public static void main(String[] args) {
+Colletion01 col=new Colletion01();
+
+col.addInc(0,10);
+col.addInc(1,20);
+col.addInc(2,30);
+col.addInc(3,40);
+col.addInc(4,50);
+        System.out.println(col);
+col.addInc(60);
+col.addInc(70);
+col.addInc(80);
+        System.out.println(col);
+col.remDec((Integer)10);
+col.remDec((Integer)80);
+        System.out.println(col);
+col.remDec(6);
+        System.out.println(col);
+col.find(3);
+col.find((Integer)60);
+
+col.aver();
+Collections.max(col);
+Collections.min(col);
+
+System.out.println(col);
+    }
+}
